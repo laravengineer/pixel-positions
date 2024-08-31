ecb39a62620d38a47411d906c8d6b712ffeec408
Eduardo Alexandre <laravengineer@gmail.com>
2024-08-31 20:57:56 -0300

    build: add tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,18 +7,25 @@
     <title>Pixel Positions</title>
     @vite(['resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="{{ route('home') }}">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
-            <div></div>
-            <div></div>
+            <div class="space-x-6 font-bold">
+                <a href="#">{{ __('Jobs') }}</a>
+                <a href="#">{{ __('Careers') }}</a>
+                <a href="#">{{ __('Salaries') }}</a>
+                <a href="#">{{ __('Companies') }}</a>
+            </div>
+            <div>
+                <a href="">{{ __('Post a Job') }}</a>
+            </div>
         </nav>
-        <main>
+        <main class="mt-10 max-w-[986px]">
             {{ $slot }}
         </main>
     </div>
